4b903ab5a81199fac3ff75e236fcd125b0523d4b
Daniel Zotti <daniel.zotti87@gmail.com>
2019-07-25 16:30:31 +0200

    Fix repo

--- projects/ng-textarea-autoresize/src/public-api.ts
+++ projects/ng-textarea-autoresize/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ng-textarea-autoresize
  */
 
-export * from './lib/ng-textarea-autoresize.service';
-export * from './lib/ng-textarea-autoresize.component';
+export * from './lib/ng-textarea-autoresize.directive';
 export * from './lib/ng-textarea-autoresize.module';
